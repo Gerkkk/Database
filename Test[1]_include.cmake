111cc38d97709d5b412e2505d7d7f17b0f41fd1b
Danila <danpol2005@gmail.com>
2024-12-03 21:52:39 +0300

    fixing bugs

--- Test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/c/Users/danpo/OneDrive/Рабочий стол/working db/Database/Test[1]_tests.cmake")
-  include("/mnt/c/Users/danpo/OneDrive/Рабочий стол/working db/Database/Test[1]_tests.cmake")
-else()
-  add_test(Test_NOT_BUILT Test_NOT_BUILT)
-endif()
